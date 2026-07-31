94f3d5d8df09b04090351898283c53635f1a8e4d
suryaprakashreddyveeramreddy <suryaprakash0115@gmail.com>
2026-07-31 15:38:20 +0530

    Updated project to use JSON Server

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import AppNavigator from "./src/navigation/AppNavigator";
 import { CartProvider } from "./src/context/CartContext";
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <CartProvider>
+       
       <AppNavigator />
+      <StatusBar hidden />
+      
     </CartProvider>
   );
 }
